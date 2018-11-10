12a4d024063405136689c15ef3dc338984a80397
Amir Dehghan <amirmd76@gmail.com>
2018-11-10 10:21:14 +0330

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+readme = "Charzeh Python (CPy)"
+
+setup(
+    name='cpy',
+    version='1.0.0',
+    description='Charzeh Python Helpers',
+    long_description=readme,
+    author='Amir Dehghan',
+    author_email='dehghan@mit.edu',
+    url='https://github.com/amirmd76/cpy.git',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
